6b41af60c54007e1b362da606e2b91d8b42aa5f3
FGoosev <62888244+FGoosev@users.noreply.github.com>
2022-12-20 13:45:59 +0300

    Add files via upload

---
+++ RabbitMq/Consumer/Program.cs
@@ -0,0 +1,48 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mongoClient = new MongoClient("mongodb://localhost:27017");
+            var db = mongoClient.GetDatabase("bigData");
+            var collectionStudents = db.GetCollection<BsonDocument>("Students");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "dev-q",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null
+                    );
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (sender, e) =>
+                {
+                    var body = e.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+                    var bson = BsonDocument.Parse(message);
+                    collectionStudents.InsertOne(bson);
+                    Console.WriteLine("Get message: {0}", message);
+                };
+                channel.BasicConsume(queue: "dev-q",
+                                     autoAck: true,
+                                     consumer: consumer
+                    );
+
+                Console.WriteLine("Subscribed to the queue dev-q");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitMq/Publisher/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using MongoDB.Bson;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+            var random = new Random();
+            var ages = new List<int> { 17,18,19,20 };
+            var facults = new List<string> { "InPIT", "InETIP", "FTI", "URBAS" };
+            var names = new List<string> { "Ivan", "Petr", "Stepan", "Anna" };
+            var surnames = new List<string> { "Petrov", "Ivanov", "Prodov", "Gresulov" };
+            do
+            {
+                int timeToSlip = new Random().Next(1000,3000);
+                Thread.Sleep(timeToSlip);
+                int indexAge = random.Next(ages.Count);
+                int indexName = random.Next(names.Count);
+                int indexSurname = random.Next(surnames.Count);
+                int indexFacult = random.Next(facults.Count);
+
+                var document = new BsonDocument()
+                {
+                    { "name", "MongoDB" },
+                    { "type", "Database" },
+                    { "count", 1 },
+                    { "info", new BsonDocument
+                    {
+                        { "Name", names[indexName] },
+                        { "Surname", surnames[indexSurname] },
+                        { "Age", ages[indexAge] },
+                        { "University", "SSTU" },
+                        { "faculty", facults[indexFacult] }
+                    }}
+                };
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "dev-q",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null
+                        );
+                    
+
+                    var body = Encoding.UTF8.GetBytes(document.ToString());
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "dev-q",
+                                         basicProperties: null,
+                                         body: body
+                        );
+
+                    Console.WriteLine("Message publish");
+                }
+            } while (true);
+            
+        }
+    }
+}
